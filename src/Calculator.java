50a0a451932c830825e56fb87c3bb3ffe4331d72
A.Rahman <46765153+urumqii@users.noreply.github.com>
2019-02-01 16:51:38 -0500

    corrected spelling error in multi method

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
     public static double sub(double num1,double num2){
         return num1-num2;
     }
-    public static double muti(double num1,double num2){
+    public static double multi(double num1,double num2){
     
 return num1*num2;    
     }
